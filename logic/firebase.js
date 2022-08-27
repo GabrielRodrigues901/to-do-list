0f75f2982b27c1eed71b6f6f59d7c4946034a6e4
Gabriel Rodrigues <92596916+GabrielRodrigues901@users.noreply.github.com>
2022-08-27 10:35:34 -0300

    minha to do list

---
+++ logic/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCe8VJykfco-qB_sJtIHHfJshuglLcoOjA",
+    authDomain: "to-do-list-4bbf0.firebaseapp.com",
+    databaseURL: "https://to-do-list-4bbf0-default-rtdb.firebaseio.com",
+    projectId: "to-do-list-4bbf0",
+    storageBucket: "to-do-list-4bbf0.appspot.com",
+    messagingSenderId: "441088485874",
+    appId: "1:441088485874:web:4ce5d5371e7c368060a36b"
+  
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
